8b61bb3ff316338f51ba4dc423f63216630bc741
Nicolay Adamchuk <87581237+NicolayAdamchuk@users.noreply.github.com>
2022-06-19 17:55:33 +0200

    Текущее состояние на 19.06.22

---
+++ ShellAllSketch/BandApp.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+using Autodesk; 
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+
+namespace ShellAllSketch
+{
+    /// <summary>
+    /// Implements the Revit add-in interface IExternalApplication
+    /// </summary>
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class ShellAllSketch : IExternalApplication
+    {
+        //public static AreaDirect areaDirect = AreaDirect.Main;
+        //public static AreaLayer areaLayer = AreaLayer.Up;
+
+        static string AddInPath = typeof(ShellAllSketch).Assembly.Location;
+        // Button icons directory
+        static string ButtonIconsFolder = Path.GetDirectoryName(AddInPath);
+        static string AddInPathSketchFull = ButtonIconsFolder + "\\SketchFull_2023.dll";
+        static string AddInPathSketchScheduler = ButtonIconsFolder + "\\SketchReinforcement_2023.dll";
+        static string AddInPathSketchTag = ButtonIconsFolder + "\\SketchTag_2023.dll";
+
+        #region IExternalApplication Members
+        /// <summary>
+        /// Implements the OnShutdown event
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Implements the OnStartup event
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnStartup(UIControlledApplication application)
+        {            
+            Autodesk.Revit.ApplicationServices.LanguageType lt = application.ControlledApplication.Language;
+            
+            if (lt.ToString() == "Russian") Resourses.Strings.Texts.Culture= new System.Globalization.CultureInfo("ru-RU");
+
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(Resourses.Strings.Texts.NamePanel);
+            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+#if Full
+            PushButtonData styleSettingButton = new PushButtonData("DatumStyle", Resourses.Strings.Texts.NameImageSketchFull, AddInPathSketchFull, "SketchFull.SketchCommand");
+            styleSettingButton.LargeImage = new BitmapImage(new Uri(Path.Combine(ButtonIconsFolder + "\\Resources\\Images\\", "RebarDrawing.png"), UriKind.Absolute)); ;
+
+            PushButtonData styleSettingButton2 = new PushButtonData("DatumStyle2", Resourses.Strings.Texts.NameImageSketchReinforcement, AddInPathSketchScheduler, "SketchReinforcement.SketchCommand");
+            styleSettingButton2.LargeImage = new BitmapImage(new Uri(Path.Combine(ButtonIconsFolder + "\\Resources\\Images\\", "RebarSchedule.png"), UriKind.Absolute)); ;
+                                   
+            //string path = "c:\\Users\\Public\\Documents\\Autodesk\\Downloaded Content\\Ar-Cadia\\SketchReinforcement";
+            ContextualHelp ch = new ContextualHelp(ContextualHelpType.ChmFile, path + Resourses.Strings.Texts.pathToHelpSchedule);
+            styleSettingButton.SetContextualHelp(ch);             
+            ribbonPanel.AddItem(styleSettingButton);
+
+            ContextualHelp ch2 = new ContextualHelp(ContextualHelpType.ChmFile, path + Resourses.Strings.Texts.pathToHelpSchedule);
+            styleSettingButton2.SetContextualHelp(ch2);             
+            ribbonPanel.AddItem(styleSettingButton2);
+#endif
+
+#if Tag
+
+            PushButtonData styleSettingButton3 = new PushButtonData("DatumStyle3", Resourses.Strings.Texts.NameImageSketchTag, AddInPathSketchTag, "SketchTag.SketchCommand");
+            styleSettingButton3.LargeImage = new BitmapImage(new Uri(Path.Combine(ButtonIconsFolder + "\\Resources\\Images\\", "RebarTag.png"), UriKind.Absolute)); ;
+            ContextualHelp ch3 = new ContextualHelp(ContextualHelpType.ChmFile, path + Resourses.Strings.Texts.pathToHelpTag);
+            styleSettingButton3.SetContextualHelp(ch3);
+            ribbonPanel.AddItem(styleSettingButton3);
+
+#endif 
+
+            return Result.Succeeded;
+        }
+
+        #endregion
+    }
+}
---
+++ ShellAllSketch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShellAllSketch")]
+[assembly: AssemblyDescription("Данное приложение выполняет создание эскизов для спецификаций и чертежей")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ar-Cadia")]
+[assembly: AssemblyProduct("ShellAllSketch")]
+[assembly: AssemblyCopyright("Copyright 2023 © Ar-Cadia")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40B0FCBD-4029-4AC1-9F2D-9B586CB0B108")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2023.0.0.0")]
+[assembly: AssemblyFileVersion("2023.0.0.0")]
